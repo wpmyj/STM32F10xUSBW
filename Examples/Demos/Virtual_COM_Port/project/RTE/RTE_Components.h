86d0ac26c9677281a18f84bf9352a6aeb5e6f402
ZhiHua Lai <rush495075830@live.com>
2017-06-20 01:59:41 +0800

    1010
    10

---
+++ Examples/Demos/Virtual_COM_Port/project/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'VirtualCOMPort' 
+ * Target:  'PM001V20' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
